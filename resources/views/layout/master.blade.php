1b6c01b80405185e08f4a936ea057e28e4b8581c
Ahmad-Khallaf05 <ahmadmajedkhallaf470@gmail.com>
2024-08-18 22:18:09 +0300

    Initial commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,52 @@
+@include ('inclouds.head');
+
+<body class="animsition">
+    <div class="page-wrapper">
+
+        @include ('inclouds.nav');
+
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+
+            @include ('inclouds.HEADER-DESKTOP');
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+
+            @yield('body')
+    
+            </div>
+            <!-- END MAIN CONTENT-->
+            <!-- END PAGE CONTAINER-->
+        </div>
+
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="vendor/slick/slick.min.js">
+    </script>
+    <script src="vendor/wow/wow.min.js"></script>
+    <script src="vendor/animsition/animsition.min.js"></script>
+    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
